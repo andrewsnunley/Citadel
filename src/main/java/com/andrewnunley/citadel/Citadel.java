4b225a4fa12fd5a4b7c4f5e5bf0432bb89dd17b7
Andrew Nunley <andrewsnunley@outlook.com>
2018-10-28 17:14:16 -0400

    Reworked initial branch

---
+++ src/main/java/com/andrewnunley/citadel/Citadel.java
@@ -0,0 +1,15 @@
+package com.andrewnunley.citadel;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Citadel extends JavaPlugin {
+    @Override
+    public void onEnable() {
+       // startup
+    }
+   
+    @Override
+    public void onDisable() {
+	   // cleanup
+    }
+}
